89cea5fd88a9050a1c4f4e776e6b0bca71975767
Leonardo L Santos <leonardoliveasm@gmail.com>
2024-08-13 15:20:43 -0300

    Desafio-Conluido!!!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -5,6 +5,8 @@ namespace DesafioProjetoHospedagem.Models
         public List<Pessoa> Hospedes { get; set; }
         public Suite Suite { get; set; }
         public int DiasReservados { get; set; }
+        public int QuantidadeDeHospedes { get; set; }
+
 
         public Reserva() { }
 
@@ -15,16 +17,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // *IMPLEMENTADO*
+            if (Suite.Capacidade >= ObterQuantidadeHospedes())// verificar, "Suite.Capacidade" é maior ou igual ao meu metodo "ObterQuantidadeHospedes()"? caso a condição for verdadeira iniciará. Caso seja falsa irá me retornar uma exceção
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // *IMPLEMENTADO* Caso a condição for falsa retorna uma Exceção
+                throw new Exception("A capacidade de hóspedes não pode exceder a capacidade da suite");
             }
         }
 
@@ -35,23 +36,32 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // *IMPLEMENTADO AQUI*
+            QuantidadeDeHospedes = 12; //Quantidade de Hospedes!
+            return QuantidadeDeHospedes; //Retorna a quantidade de Hospedes!
+            
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
+            // *IMPLEMENTADO AQUI*
             decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            valor = DiasReservados * Suite.ValorDiaria; //Calculo do valor geral
+            // *IMPLEMENTADO AQUI*
+            if (DiasReservados >= 10) // Aqui está verificando. DiasReservados é maior ou igual a 10? Se a condição for verdadeira, ganhará 10% de desconto!
             {
-                valor = 0;
+                valor -= valor * 0.10m; // Calculo com 10% de desconto
+                /*
+                    Calculo aplicado com desconto.
+                    Explicação: Estou pegando minha variavel "valor", é estou subtraindo "valor - valor", multiplicada por 0,10 que é 10%
+                    minha variável, "valor" é atribuída o resultado da multiplicação, de DiasReservados x Suite.ValorDiaria.
+                    Com o resultado da multiplicação atribuida no valor, agora irei subtrair por ela mesma sendo assim.
+                    valor -= valor x 0.10 = 270.
+                    no momento em que chamo minha variável valor, ela está armazenando o valor 300 pois DiasReservados = 10 ValorDiaria = 30.
+                    Então 30 x 10 = 300, é valor x 0.10 = 30 sendo assim 300 - 30 = 270 
+                    obs: valor é = 300 por isso, valor x 0.10 = 30!
+                */
+
             }
 
             return valor;
